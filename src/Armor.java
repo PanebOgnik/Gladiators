7ba723cfd1548d5233aaf33cc60e688274826ac5
paneb <panebognik33@gmail.com>
2024-12-08 12:31:21 +0100

    Start a small gladiator fight project written in Java.

--- src/Armor.java
+++ src/Armor.java
@@ -3,4 +3,11 @@ public class Armor {
     int level;
     int defense;
     int price;
+
+    public Armor(String name, int level, int defense, int price) {
+        this.name = name;
+        this.level = level;
+        this.defense = defense;
+        this.price = price;
+    }
 }
--- src/Weapon.java
+++ src/Weapon.java
@@ -4,4 +4,12 @@ public class Weapon {
     int attackSpeed;
     int level;
     int price;
+
+    public Weapon(String name, int damage, int attackSpeed, int level, int price) {
+        this.name = name;
+        this.damage = damage;
+        this.attackSpeed = attackSpeed;
+        this.level = level;
+        this.price = price;
+    }
 }
